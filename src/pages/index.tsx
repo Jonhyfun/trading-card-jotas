640378030288c6d078ef579786bcd4a43fdaee67
João Paulo Grimaldi <jp.grimaldi@nv99.com.br>
2024-04-03 00:49:32 -0300

    feat: typewriter

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,4 @@
+import Typewriter from 'react-ts-typewriter';
 import { Press_Start_2P } from "next/font/google";
 import { hexToRgb, pixelBorder, toggleCRT } from "@/styles/utils";
 import { Palette } from "../../tailwind.config";
@@ -33,7 +34,9 @@ export default function Home() {
                 className="w-full h-full border-2 border-primary-light bg-bg-internal"
                 >
                 <div className="w-full h-full border-2 border-black bg-bg-internal grid grid-rows-3 p-3">
-                  <span className="pt-8">Trading Card Game do Jotas em breve!</span>
+                  <span className="pt-8">
+                    <Typewriter cursor={false} text="Trading Card Game do Jotas em breve!"/>
+                  </span>
                   <TripleBorder className="row-span-2" borderColor="gray-light">
                     <div className="w-full h-full md:p-5 md:pt-7 p-3 pt-5 flex flex-col gap-6 md:text-base text-xs">
                       <div className="group cursor-pointer"><span className="group-hover:visible invisible mr-2">*</span><span>Nova Partida</span></div>
