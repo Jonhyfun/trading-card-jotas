776f83b63392db54cfdade505c13e5f5ad8a8879
jonhyfun <jonhyfun@gmail.com>
2025-04-13 15:05:51 -0300

    fix: leaked api key lol

--- packages/trading-card-jotas-front/src/utils/firebase.ts
+++ packages/trading-card-jotas-front/src/utils/firebase.ts
@@ -1,14 +1,14 @@
-import { initializeApp } from 'firebase/app';
-import { getAuth } from 'firebase/auth';
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyA-syzN-WwskGP6ESb__lU6CFsa33kcKNU",
+  apiKey: process.env.GOOGLE_API_KEY,
   authDomain: "biggiscript.firebaseapp.com",
   databaseURL: "https://biggiscript.firebaseio.com",
   projectId: "biggiscript",
   storageBucket: "biggiscript.appspot.com",
   messagingSenderId: "761282575903",
-  appId: "1:761282575903:web:1f7e783b6d6a3f72321f2a"
+  appId: "1:761282575903:web:1f7e783b6d6a3f72321f2a",
 };
 
 const firebaseApp = initializeApp(firebaseConfig);
